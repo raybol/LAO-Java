9d96b154293eb32c38748f05b81a4bb4cfa7cb6d
raybol <felicianoraul@gmail.com>
2017-02-11 13:30:25 -0400

    ops

---
+++ LAO Interpreter/src/lao/interpreter/IntVariable.java
@@ -0,0 +1,45 @@
+/*
+ *   Polytechnic University of Puerto Rico                       
+ *   Electrical & Computer Engineering and                       
+ *        Computer Science Department                            
+ *                                                                
+ *       CECS3210 Advanced Programming  
+ */
+
+package lao.interpreter;
+
+/**
+ * @author Raul Feliciano &lt;felicianoraul@gmail.com&gt;
+ */
+public class IntVariable extends Variable {
+    
+    private int value;
+    private char type;
+
+    
+    public IntVariable(String identifier) {
+        super(identifier);
+        type='i';
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public char getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(value);
+    }
+
+
+    
+
+}
